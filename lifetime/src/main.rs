773c4d5f6f69ecce15a918a3c62bed8d8e5c5449
leolanger <1176127450@qq.com>
2020-09-24 20:10:02 +0800

    push

--- lifetime/src/main.rs
+++ lifetime/src/main.rs
@@ -1,3 +1,4 @@
+use std::fmt::Display;
 fn main() {
     let x = 5;
     let r = &x;
@@ -13,6 +14,18 @@ fn main() {
 }
 
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement! {}", ann);
     if x.len() > y.len() {
         x
     } else {
